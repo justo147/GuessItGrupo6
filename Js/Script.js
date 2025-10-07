1d1961608acb3bb5fdafcc67e6a5ac9c7ed44202
DESKTOP-HHGOI4D\Justo <justo147@hotmail.com>
2025-10-07 10:38:05 +0200

    feat(script):añadida funcionalidad de contador

--- Js/Script.js
+++ Js/Script.js
@@ -1,6 +1,7 @@
 let num;
 let adivinado=false;
 let numRand=random(1,100);
+let contador=0;
 do{
     num=parseInt(prompt("Escribe un numero"));
     if(num<numRand){
@@ -11,9 +12,16 @@ do{
         alert("Felicidades has adivinado el numero");
         adivinado=true;
     }
-
+    contador++;
 }while(adivinado!=true);
 
+if(contador<=5){
+    document.writeln("Excelente has tenido "+contador+" Intentos");
+}else if(contador>5 && contador<=10){
+    document.writeln("Bien has tenido "+contador+" Intentos");
+}else{
+    document.writeln("Regular has tenido "+contador+" Intentos");
+}
 
 //Funcion para calcular el numero random entero
 function random(min, max) {
