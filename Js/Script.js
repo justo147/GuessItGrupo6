c709d35b51f1ad1f386a2e0d9081f5b009d14920
Sergio <spermor510a@isidrodearceneguiycarmona.es>
2025-10-07 12:05:32 +0200

    fix(script): corregir variable de contador y mejorar mensajes de salida

--- Js/Script.js
+++ Js/Script.js
@@ -1,29 +1,29 @@
 let num;
-let adivinado=false;
-let numRand=random(1,100);
-let contador=0;
-do{
-    num=parseInt(prompt("Escribe un numero"));
-    if(num<numRand){
+let guess = false;
+let numRand = random(1, 100);
+let count = 0;
+do {
+    num = parseInt(prompt("Escribe un numero"));
+    if (num < numRand) {
         alert("El numero a adivinar es mayor");
-    }else if(num>numRand){
+    } else if (num > numRand) {
         alert("El numero a adivinar es menor");
-    }else{
+    } else {
         alert("Felicidades has adivinado el numero");
-        adivinado=true;
+        guess = true;
     }
-    contador++;
-}while(adivinado!=true);
+    count++;
+} while (guess != true);
 
-if(contador<=5){
-    document.writeln("Excelente has tenido "+contador+" Intentos");
-}else if(contador>5 && contador<=10){
-    document.writeln("Bien has tenido "+contador+" Intentos");
-}else{
-    document.writeln("Regular has tenido "+contador+" Intentos");
+if (count <= 5) {
+    document.writeln("Excelente, lo has adivinado en " + count + " intentos");
+} else if (count > 5 && count <= 10) {
+    document.writeln("Bien, lo has adivinado en " + count + " intentos");
+} else {
+    document.writeln("Regular, lo has adivinado en " + count + " intentos");
 }
 
 //Funcion para calcular el numero random entero
 function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 } 
